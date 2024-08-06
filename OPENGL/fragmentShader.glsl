44512a8723d725ac74099cfc2da37abdb2252e79
Arthur020104 <arthur020104@gmail.com>
2024-08-06 20:41:05 -0300

    Adicionando shader de teste

--- OPENGL/fragmentShader.glsl
+++ OPENGL/fragmentShader.glsl
@@ -11,6 +11,33 @@ void main()
     gl_FragColor = vec4(red, green, blue, 1.0f);
 }
 
+
+/*TESTE SHADER
+//FRAGMENT SHADER
+#version 330 core
+uniform float timeValue;
+in vec3 position;
+in vec3 colorFrag;
+uniform float circleTickness;
+void main()
+{
+    float aux = pow(position.x, 2) + pow(position.y, 2);
+    float timeIntervalZeroAndOne = mod(timeValue, 2);
+    bool isInCircle = ((aux) <= pow(timeIntervalZeroAndOne/10, 2) && (aux) >= pow(timeIntervalZeroAndOne/10 - circleTickness, 2)) || (aux) <= pow((mod(timeValue, 3)/10), 2) && (aux) >= pow((mod(timeValue, 3)/10) - circleTickness, 2) || (aux) <= pow((mod(timeValue, 4)/10), 2) && (aux) >= pow((mod(timeValue, 4)/10) - circleTickness, 2);
+    if(isInCircle)
+    {
+        gl_FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+        return;
+    }
+    float red = (cos(timeValue) + position.x + position.y + colorFrag.x)/4.0f + 0.5f; 
+    float green = (sin(timeValue) + position.x + position.y + colorFrag.y)/4.0f + 0.5f;
+    float blue = (sin(timeValue) + position.x + position.y + colorFrag.z)/4.0f + 0.5f;
+    gl_FragColor = vec4(red, green, blue, 1.0f);
+}
+
+*/
+
+
 /*
 -Exercicio de cor baseado no posicao
 in vec3 position;
