3f6127eff7aa0ac422933614b5098314dbf9329a
Arthur020104 <arthur020104@gmail.com>
2024-08-13 20:07:06 -0300

    Testando aplicar shaders diferentes no mesmo programa

--- OPENGL/OPENGL.cpp
+++ OPENGL/OPENGL.cpp
@@ -7,7 +7,7 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
-unsigned int VBO, VAO, EBO, VBO1, VAO1;
+unsigned int VBO, VAO, EBO, VBO1, VAO1, EBO1;
 unsigned int texture1, texture2;
 int main()
 {
@@ -47,16 +47,17 @@ int main()
     /*******************Set GLAD********************/
 
     glViewport(0, 0, WIDTH, HEIGHT);
-
+    Shader defaultShader("C:/Users/arthu/OneDrive/Desktop/OpenGl/OPENGLCOM/OPENGL/OPENGL/Shaders/vertexShaderDefault.glsl", "C:/Users/arthu/OneDrive/Desktop/OpenGl/OPENGLCOM/OPENGL/OPENGL/Shaders/fragmentShaderDefault.glsl");
     Shader myShaders("C:/Users/arthu/OneDrive/Desktop/OpenGl/OPENGLCOM/OPENGL/OPENGL/Shaders/vertexShader.glsl", "C:/Users/arthu/OneDrive/Desktop/OpenGl/OPENGLCOM/OPENGL/OPENGL/Shaders/fragmentShader.glsl");
 
     myShaders.use();
 
-    glUniform1i(glGetUniformLocation(myShaders.ID, "texture1"), 0);
+    myShaders.setInt("texture1", 0);
     myShaders.setInt("texture2", 1);
 
+    
+    house(VAO1, VBO1, EBO1);
     retangle(VAO, VBO, EBO, texture1, texture2);
-
     while (!glfwWindowShouldClose(window))
     {
 
@@ -64,8 +65,9 @@ int main()
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        myShaders.use();
+        
+        
+        
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -74,23 +76,30 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
         
 
-        float timeValue = glfwGetTime();
+        
 
+        
+        glBindVertexArray(VAO);
+        float timeValue = glfwGetTime();
+        myShaders.use();
         myShaders.setFloat("timeValue", timeValue);
         myShaders.setFloat("mixValue", 0.2f);
-
-        glBindVertexArray(VAO);
-
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);// glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(VAO1);
+        defaultShader.use();
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-
+    glDeleteBuffers(1, &VAO1);
+    glDeleteBuffers(1, &VBO1);
+    glDeleteBuffers(1, &EBO1);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
--- OPENGL/Shapes.cpp
+++ OPENGL/Shapes.cpp
@@ -190,16 +190,16 @@ void triangles(unsigned int& VAO, unsigned int& VBO, const int& vertices)
 void house(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO)
 {
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f,   // top left 
-        0.0f, 1.0f, 0.0f//top
+        -1.0f, -1.0f, 0.0f,
+        -0.5f, -1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -1.0f, -0.5f, -0.0f,
+        -0.75f, -0.25f, 0.0f
     };
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3,   // second triangle
-        0, 3 ,4
+        0, 1, 2,   // first triangle
+        0, 2, 3,   // second triangle
+        2, 3 ,4
     };
 
     glGenVertexArrays(1, &VAO);
